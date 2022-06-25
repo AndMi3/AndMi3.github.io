1f8cd2c91b6738f3d985c69bb0542e8031d9462c
AndMi3 <85646602+AndMi3@users.noreply.github.com>
2022-06-25 11:40:34 +0300

    ❤
    Любвю сьвой тьвитотек

--- Timer/script.js
+++ Timer/script.js
@@ -8,7 +8,6 @@ function reload(){
   date.D = document.querySelectorAll('span.time')[0].innerText;      
   date.M = document.querySelectorAll('span.time')[1].innerText;
   date.Y = document.querySelectorAll('span.time')[2].innerText;
-  console.log(date.fdtto);
 }
 
 /*
