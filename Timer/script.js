b0b044c487158b3143b788be8a81a8f0455edce1
AndMi3 <85646602+AndMi3@users.noreply.github.com>
2022-06-26 07:25:34 +0300

    Update script.js

--- Timer/script.js
+++ Timer/script.js
@@ -1,22 +1,33 @@
+// создаём переменную, куда будем заносить результаты вычислений
 let res = {};
 
+// создаём переменную для тэга, в который буднм выводить сам результат
 let first;
 
+// а это интервал для обновления данных
 let interval = setInterval(reload, 1000);
 
+//создаём переменную для входящих данных времени в секундах
 let date = {
-  'fdtto': `${document.querySelector('span#fdtto').innerText}`
+  'fdtto': `${document.querySelector('span#fdtto').innerText}`;
 }
 
+// добавляем функцию как добавления данных, так и их обновления
 function reload(){                   
 
+  // это так называемая "отправная точка", то есть 14 декабря 2021 года, имеет постоянное значение, приводим её значение к числовому формату
   date.fdtto = Number(date.fdtto);
+  // тут в консоль выводим её значение
   console.log(`Отправная точка: ${date.fdtto}`);
 
+  // а это, собственно секунды с 1 января 1970 года до сегодняшнего дня
   date.realtime = document.querySelectorAll('span.time')[0].innerText;
+  // тут так же приводим к числовому формату
   date.realtime = Number(date.realtime);
+  // и выводим в консоль значение, но это не обязательно, я её чекал просто
   console.log(`реальное время: ${date.realtime}`);
 
+  // а тут самое интнресное  начинаем подсчёт данных, зная "отправную точку" и сегодняшний момент  функция смешная  Math.trunc (маф труньк) аххахахаххах  в общем она избавляет числа от мноооожества имволов после запятой
   res.years = `${Math.trunc((date.realtime - date.fdtto) / 60 / 60 / 24 / 30 / 12)}`;
   res.month = `${Math.trunc((date.realtime - date.fdtto) / 60 / 60 / 24 / 30)}`;
   res.weeks = `${Math.trunc((date.realtime - date.fdtto) / 60 / 60 / 24 / 7)}`;
@@ -25,6 +36,7 @@ function reload(){
   res.minutes = `${Math.trunc((date.realtime - date.fdtto) / 60)}`;
   res.seconds = `${Math.trunc(date.realtime - date.fdtto)}`;
 
+  // и здесь, собственно, ищем поле вывода данных и выводим на экран значения
   first = document.querySelector('span#first');
   first.innerText = `\n\nМы вмефти: \n${res.years} год/года/лет\n${res.month} месяц/месяца/месяцев\n${res.weeks} неделю/недели/недель\n${res.days} день/дня/дней\n${res.hours} час/часа/часов\n${res.minutes} минуту/минуты/минут\n${res.seconds} секунду/секунды/секунд`;
 
